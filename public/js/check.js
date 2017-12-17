4cf0c740f8c82e7a223ca69a5b8c4369d14e0088
RemyMozelle <timoz974@hotmail.fr>
2017-12-17 20:52:34 +0400

    change Ok

---
+++ public/js/check.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  /* const login = $('#login').click((e)=>{
+    e.preventDefault()
+      console.log(login[0].email.value)
+  }) */
+
+  console.log($('#email').val())
+});
