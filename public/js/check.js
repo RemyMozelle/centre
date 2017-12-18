7295626b40319751dcfe8f09a3586744aba93aed
RemyMozelle <timoz974@hotmail.fr>
2017-12-18 07:57:36 +0400

    loggin logout Ok, ajout du menu parti admin

--- public/js/check.js
+++ public/js/check.js
@@ -1,8 +1,12 @@
-$(document).ready(function(){
-  /* const login = $('#login').click((e)=>{
-    e.preventDefault()
-      console.log(login[0].email.value)
-  }) */
+/* $(document).ready(function(){
+  const email = $('#email').val()
+  const password = $('#password').val()
 
-  console.log($('#email').val())
-});
+  $("#login").submit((e)=>{
+    if(email === '') { console.log('ce champ et vide');}
+    if(password === '') { console.log('ce champ et password est vide');}
+    $('#error').append('<p class="alert alert-danger">erreur</p>');
+  })
+
+  
+}); */
