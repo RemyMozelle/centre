56c42c2e372bf8cf6028c883dba6bf08cee75434
RemyMozelle <timoz974@hotmail.fr>
2017-12-21 14:51:50 +0400

    jawsDB

--- config/database/db.js
+++ config/database/db.js
@@ -3,13 +3,15 @@ const dotenv = require('dotenv').config({ path: '.env' })
 
 const db = {
   getDb(){
-    const connection = mysql.createConnection({
+    /* const connection = mysql.createConnection({
       host: 'localhost',
       user: 'root',
       password: 'root',
       database: 'centrev2'
     })
     
+    return connection */
+    const connection = mysql.createConnection(process.env.JAWSDB_URL)
     return connection
   }
 }
